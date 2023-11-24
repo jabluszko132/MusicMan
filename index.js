4e1d0ef4eca896068b8162cd1cceae73ea6154e3
Jan Stanisław Nowacki <123495481+jabluszko132@users.noreply.github.com>
2023-11-24 11:06:08 +0100

    Fix interactionCreate eventhandler
    By deleting an unnecessary 'if' condition that was never met

--- index.js
+++ index.js
@@ -1,28 +1,20 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
-// Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-
-
-//#region command handling
 client.commands = new Collection();
-
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
-
 	const commandsPath = path.join(foldersPath, folder);
 	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-
 	for (const file of commandFiles) {
 		const filePath = path.join(commandsPath, file);
 		const command = require(filePath);
-		// Set a new item in the Collection with the key as the command name and the value as the exported module
 		if ('data' in command && 'execute' in command) {
 			client.commands.set(command.data.name, command);
 		} else {
@@ -30,8 +22,6 @@ for (const folder of commandFolders) {
 		}
 	}
 }
-
-//#endregion
 
 const eventsPath = path.join(__dirname, 'events');
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
@@ -46,8 +36,4 @@ for (const file of eventFiles) {
 	}
 }
 
-
-
-
-// Log in to Discord with your client's token
 client.login(token);
