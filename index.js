052bee873ef4dafce4a3c5d624951d29f084ad86
Jan Stanisław Nowacki <123495481+jabluszko132@users.noreply.github.com>
2023-11-24 12:54:45 +0100

    Added dependencies

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
+const { Player } = require('discord-player');
+
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
